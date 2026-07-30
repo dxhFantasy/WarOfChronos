e448929314a2422a142a37916104f8b017b4fd5c
tomabow <tomabow@qq.com>
2026-07-30 20:46:32 +0800

    游戏框架0.7

--- game/classes/Target.py
+++ game/classes/Target.py
@@ -1,6 +1,15 @@
 from classes.Card import *
 from classes.Tags import *
+from enum import Enum
 
+class UnitType(Enum):
+  infantry = '步兵'
+  tank = '坦克'
+  fighter = '战斗机'
+  bomber = '轰炸机'
+  starship = '星舰'
+
+'''
 
 class Target(Card):
   def __init__(self, attack_ : int,defense_ : int, tags_ : list[Tag], cost_ : int, name_ : str, owner_ : str) -> None:
@@ -8,6 +17,24 @@ class Target(Card):
     self.defense = defense_
     self.attack = attack_
     self.id : int | None = None
+'''
+
+
+class UnitCard(Card):
+  def __init__(self, attack_: int, defense_: int, tags_: list[Tag], cost_ : int, actionCost_ : int, name_ : str, owner_ : str,type_ : UnitType, inHand_ : bool = True) -> None:
+    super().__init__(cost_, tags_, name_, owner_)
+    self.defense = defense_
+    self.attack = attack_
+    self.id : int | None = None
+    self.actionCost = actionCost_
+    self.inHand = inHand_
+    self.type = type_
+  
+  def AddAttack(self,n : int):
+    self.attack += n
+  
+  def AddTag(self,newTag : Tag):
+    self.tags.append(newTag)
 
   def loseHp(self, damage : int) -> tuple[int,int]:
     self.defense-=damage
@@ -24,19 +51,6 @@ class Target(Card):
   
 
 
-class Unit(Target):
-  def __init__(self, attack_: int, defense_: int, tags_: list[Tag], cost_ : int, actionCost_ : int, name_ : str, owner_ : str, inHand_ : bool = True) -> None:
-    super().__init__(defense_, attack_, tags_, cost_, name_, owner_)
-    self.actionCost = actionCost_
-    self.inHand = inHand_
-  
-  def AddAttack(self,n : int):
-    self.attack += n
-  
-  def AddTag(self,newTag : Tag):
-    self.tags.append(newTag)
-
-
-class Command(Card):
+class CommandCard(Card):
   def __init__(self, cost_: int, tags_: list[Tag], name_ : str, owner_ : str) -> None:
     super().__init__(cost_, tags_, name_, owner_)
