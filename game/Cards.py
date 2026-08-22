9666b41f4b9c6687da78abadfab37140182d2dc1
tomabow <537702200@qq.com>
2026-08-22 12:39:36 +0800

    hyw

--- game/Cards.py
+++ game/Cards.py
@@ -1,5 +1,4 @@
 from .classes.Target import *
-from .classes.Command import *
 from dataclasses import dataclass
 import random
 
@@ -8,9 +7,9 @@ B = 1
 C = 2
 
 allCards : list[UnitCard | CommandCard] = [
-UnitCard(6,8,[Tag(Keyword.Guard),Tag(Keyword.Prepared)],6,2,'ZTZ99A主战坦克','N',UnitType.tank,B),
-UnitCard(6,2,[Tag(Keyword.Blitz)],\
-            3,1,'2S38\"偏瘫\"','N',UnitType.tank,B),
+UnitCard(6,7,[Tag(Keyword.Guard),Tag(Keyword.Prepared)],7,2,'ZTZ99A主战坦克','N',UnitType.tank,B),
+UnitCard(4,2,[Tag(Keyword.Blitz)],\
+            3,1,'2S38\"偏流\"','N',UnitType.tank,B),
 CommandCard(7,[],'破釜沉舟','',effects_=[
   EffectData(
     target=TargetChoose(
@@ -60,12 +59,92 @@ CommandCard(7,[],'死线扩散','',effects_=[
     value = -2,
   ),
 ],dect_='使所有敌方单位攻击力为 0 ,友方失去2个行动点槽',tl=C),
-
+UnitCard(
+  attack_= 5,
+  defense_= 4,
+  tags_= [
+    Tag(
+      keyword=Keyword.Blitz
+    ),
+    Tag(
+      keyword=Keyword.Deathrattle,
+      value=[
+        EffectData(
+          target=TargetChoose(
+            owner=TargetOwner.Enemy,
+            num=1,
+            Random=False,
+            condition=ChooseCondition(
+              tid=HQ
+            )
+          ),
+          effect=EffectType.TakeDamage,
+          value=3,
+        )
+      ]
+    )
+  ],
+  cost_ = 6,
+  actionCost_ = 2,
+  name_ = '强-5 强击机',
+  owner_='N',
+  type_=UnitType.bomber,
+  timeline=B
+),
+CommandCard(
+  cost_=5,
+  tags_=[],
+  name_='陈庆之北伐',
+  owner_='',
+  effects_=[
+    EffectData(
+      target=TargetChoose(
+        owner=TargetOwner.Enemy,
+        num=ALL,
+        Random=False
+      ),
+      effect=EffectType.Buff,
+      value=(-2,-3)
+    )
+  ],
+  dect_='使所有敌方单位获得-2-3',
+  tl=A
+),
+UnitCard(
+  attack_=7,
+  defense_=5,
+  tags_=[
+    Tag(
+      keyword=Keyword.Blitz
+    ),
+  ],
+  cost_=7,
+  actionCost_=1,
+  name_='BMPT-"终结者"',
+  owner_='N',
+  type_=UnitType.tank,
+  timeline=B,
+),
+UnitCard(
+  attack_=4,
+  defense_=5,
+  tags_=[
+    Tag(
+      keyword=Keyword.Guard
+    )
+  ],
+  cost_=4,
+  actionCost_=0,
+  name_='白袍军',
+  owner_='N',
+  type_=UnitType.infantry,
+  timeline=A
+)
 ]
 
 cardIds = list(range(len(allCards)))
 
-cardIds *= 20
+cardIds *= 10
 
 def CardToHand(cid : int):
   return HandCard(cid,allCards[cid].cost,[])
